8333665d8584874eeb45b89d63f6c07e30099c07
DarkMidus <zachary.hand.is.awesome@gmail.com>
2024-06-06 01:44:34 -0500

    App now actually works. Cleaned code.

--- main.go
+++ main.go
@@ -15,106 +15,90 @@ import (
 
 func main() {
 	isPlasmaRunning()
-	systray.Run(onReady, onExit)
-	for {
-		running, _, err := checkProcessExistence("Code.exe")
-
-		if err != nil {
-			fmt.Println("Error while checking process: ", err)
-			return
-		}
-
-		if running {
-			start := time.Now()
-			for {
-				running, _, err := checkProcessExistence("Code.exe")
-
-				if err != nil {
-					fmt.Println("Error while checking process: ", err)
-					return
-				}
-				if running {
-					time.Sleep(1 * time.Second)
-				} else {
-					stop := time.Now()
-					timeSpent := stop.Sub(start)
-					hours := int(timeSpent.Hours())
-					minutes := int(timeSpent.Minutes()) % 60
-					seconds := int(timeSpent.Seconds()) % 60
-					results := " Start: " + start.Format("15:04:05") + " End: " + stop.Format("15:04:05") + fmt.Sprintf(" Time spent: %02d:%02d:%02d\n", hours, minutes, seconds)
-					documentUsage(start.Format("2006-01-02"), results)
-					break
-				}
-			}
-		} else {
-			time.Sleep(1 * time.Second)
-		}
-
-	}
-
+	go systray.Run(onReady, onExit)
+	monitorProcess("Code.exe")
 }
 
 func checkProcessExistence(name string) (bool, int, error) {
-	var processAmount int
 	cmd := exec.Command("tasklist")
 	var out bytes.Buffer
 	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
+	if err := cmd.Run(); err != nil {
 		return false, 0, err
 	}
-	output := out.String()
-	lines := strings.Split(output, "\n")
-	for _, line := range lines {
-		if strings.Contains(line, name) {
-			processAmount++
-		}
-	}
-	if processAmount > 0 {
-		return true, processAmount, nil
-	}
-	return false, 0, nil
+
+	processAmount := strings.Count(out.String(), name)
+	return processAmount > 0, processAmount, nil
 }
 
 func doesFileExist(filename string) bool {
-	if _, err := os.Stat(filename); err == nil {
+	_, err := os.Stat(filename)
+	if err == nil {
 		return true
-
-	} else if errors.Is(err, os.ErrNotExist) {
+	}
+	if errors.Is(err, os.ErrNotExist) {
 		return false
+	}
+	fmt.Println(err)
+	return false
+}
 
+func documentUsage(date string, results string) {
+	const logDir = "log"
+	const totalTimeFile = logDir + "/total.txt"
+	dailyTimeFile := logDir + "/" + date + ".txt"
+
+	// Ensure the log directory exists
+	if _, err := os.Stat(logDir); os.IsNotExist(err) {
+		if err := os.Mkdir(logDir, 0755); err != nil {
+			fmt.Println("Error creating log directory:", err)
+			return
+		}
+	}
+
+	appendToFile(dailyTimeFile, results)
+
+	timeSpentStr := strings.TrimSpace(strings.Split(results, "Time spent: ")[1])
+	timeSpent, err := time.Parse("15:04:05", timeSpentStr)
+	if err != nil {
+		fmt.Println("Error parsing time spent:", err)
+		return
+	}
+
+	if doesFileExist(totalTimeFile) {
+		totalTimeContent, err := os.ReadFile(totalTimeFile)
+		if err != nil {
+			fmt.Println("Error reading total time file:", err)
+			return
+		}
+
+		totalDuration, err := time.Parse("15:04:05", strings.TrimSpace(string(totalTimeContent)))
+		if err != nil {
+			fmt.Println("Error parsing total time:", err)
+			return
+		}
+
+		newTotalDuration := totalDuration.Add(timeSpent.Sub(time.Date(0, 1, 1, 0, 0, 0, 0, time.UTC)))
+		if err := os.WriteFile(totalTimeFile, []byte(newTotalDuration.Format("15:04:05")), 0644); err != nil {
+			fmt.Println("Error writing to total time file:", err)
+		}
 	} else {
-		println(err.Error())
-		return false
+		if err := os.WriteFile(totalTimeFile, []byte(timeSpentStr), 0644); err != nil {
+			fmt.Println("Error writing to total time file:", err)
+		}
 	}
 }
 
-func documentUsage(title string, results string) {
-	filename := "log/" + title + ".txt"
-	if doesFileExist(filename) {
-		file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		defer file.Close()
+func appendToFile(filename, data string) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
 
-		_, err = file.WriteString(results)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-	} else if !doesFileExist(filename) {
-		file, err := os.Create(filename)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		defer file.Close()
-
-		_, err = file.WriteString(results)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
+	if _, err := file.WriteString(data); err != nil {
+		fmt.Println("Error writing to file:", err)
 	}
 }
 
@@ -139,10 +123,45 @@ func onReady() {
 
 }
 
+func monitorProcess(processName string) {
+	for {
+		running, _, err := checkProcessExistence(processName)
+		if err != nil {
+			fmt.Println("Error while checking process:", err)
+			return
+		}
+
+		if running {
+			start := time.Now()
+			for {
+				running, _, err := checkProcessExistence(processName)
+				if err != nil {
+					fmt.Println("Error while checking process:", err)
+					return
+				}
+				if running {
+					time.Sleep(1 * time.Second)
+				} else {
+					stop := time.Now()
+					timeSpent := stop.Sub(start)
+					hours := int(timeSpent.Hours())
+					minutes := int(timeSpent.Minutes()) % 60
+					seconds := int(timeSpent.Seconds()) % 60
+					results := fmt.Sprintf(" Start: %s End: %s Time spent: %02d:%02d:%02d\n", start.Format("15:04:05"), stop.Format("15:04:05"), hours, minutes, seconds)
+					documentUsage(start.Format("2006-01-02"), results)
+					break
+				}
+			}
+		} else {
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
 func isPlasmaRunning() {
 	running, processAmount, err := checkProcessExistence("Plasma.exe")
 	if err != nil {
-		fmt.Println("Error while checking process: ", err)
+		fmt.Println("Error while checking process:", err)
 		return
 	}
 	if running && processAmount > 1 {
